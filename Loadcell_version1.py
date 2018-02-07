dd4a58aa8cfbe978ecedb8a51aa0947ab6a5ddf1
phelpsxia <34462089+phelpsxia@users.noreply.github.com>
2018-02-07 14:11:51 +0800

    Add files via upload

--- Loadcell_version1.py
+++ Loadcell_version1.py
@@ -2,18 +2,24 @@ import RPi.GPIO as GPIO
 import time
 import sys
 import numpy as np
+from flask import Flask, request
 from hx711 import HX711
 
+app = Flask(__name__)
+hx = ['' for i in range(4) ]
+unit_weight = [0 for i in range(4)]
+recent_data = [[0]*10 for i in range(4) ]
+count = [0 for i in range(4) ]
+total = [0 for i in range(4) ]
+flag = [0 for i in range(4) ]
+val = [0 for i in range(4) ]
+real_weight = [0 for i in range(4) ]
+unitRefer = [0 for i in range(4) ]
+unitInBasket = [0 for i in range(4) ]
+weight_refer = [0 for i in range(4) ]
+effeciency = [0 for i in range(4) ]
+global id
 
-hx = list()
-unit_weight = list()
-recent_data = list()
-count = list()
-total = list()
-flag = list()
-val = list()
-real_weight = list()
-weight_refer, unitInBasket, unitRefer = list()
 
 def cleanAndExit():
     print "Cleaning..."
@@ -53,7 +59,64 @@ def init(pin1,pin2,id):
 def most_common(lst):
     return max(set(lst), key=lst.count)
 
+def weight(id):
+    val[id] = round(hx[id].get_weight(5))
+    if id == 3:
+        print "current sample:" , val[id]
+    recent_data[id][1:10] = recent_data[id][0:9]
+    
+    if val[id] >= 0:
+        recent_data[id][0] =  val[id]
+    else:
+        recent_data[id][0] = 0
+    
+    if most_common(recent_data[id]) == 0:
+        empty = 1
+    else:
+        empty = 0
+        
+    if id == 3:
+        print "recent 10 data:", recent_data[id]
+        
+    if empty:
+        unit_weight[id] = most_common(recent_data[id][0:3])
+    
+    real_weight[id] = most_common(recent_data[id])
+    if real_weight[id] <= 1:
+        real_weight[id] = 0
+            
+    weight_refer[id] = most_common(recent_data[id][5:10])
+    if weight_refer[id] <= 1:
+        weight_refer[id] = 0
+    if id == 3:        
+        print "real weight:",real_weight[id], "refer weight:", weight_refer[id], "unit weight:", unit_weight[id]
+        
+    if unit_weight[id] > 1:
+        unitInBasket[id] = round(real_weight[id] / unit_weight[id])
+        unitRefer[id] = round(weight_refer[id] / unit_weight[id])
+    else:
+        unitInBasket[id] = 0
+        unitRefer[id] = 0
+    if id == 3:
+        print "unit in the basket:", unitInBasket[id], "refer unit:", unitRefer[id]
+        
+    if real_weight[id] > weight_refer[id] and count[id] == 0:
+        total[id] += unitInBasket[id] - unitRefer[id]
+        flag[id] = 1
+    if id == 3:        
+        print "subtotal:",total[id]
+        print "==============="
+        
+    hx[id].power_down()
+    hx[id].power_up()
+    time.sleep(0.05)
+    
+@app.route("/main/int:0", methods=['GET', 'POST'])    
 def main(id):
+    init(16,12,0)
+    init(24,23,1)
+    init(6,5,2)
+    init(13,26,3)
     while True:
     
         # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
@@ -62,55 +125,31 @@ def main(id):
         #np_arr8_string = hx.get_np_arr8_string()
         #binary_string = hx.get_binary_string()
         #print binary_string + " " + np_arr8_string
+        try:
+            if flag[id] == 1:
+                count[id] += 1
         
-        if flag[id] == 1:
-            count[id] += 1
-        
-        if count[id] == 4:
-            count[id] = 0
-            flag[id] = 0
+            if count[id] == 4:
+                count[id] = 0
+                flag[id] = 0
+            
+            weight(id)
+            id += 1
+            if id == 4:
+                id = 0
+                r = jsonify({'effieciency': {
+                    'worker1':{'total': total[0], 'the amount in the basket': unitInBasket[0], 'effeciency': effeciency[0]},
+                    'worker2':{'total': total[1], 'the amount in the basket': unitInBasket[1], 'effeciency': effeciency[1]},
+                    'worker3':{'total': total[2], 'the amount in the basket': unitInBasket[2], 'effeciency': effeciency[2]},
+                    'worker4':{'total': total[3], 'the amount in the basket': unitInBasket[3], 'effeciency': effeciency[3]}}})
+                return r
+            print "id:" , id
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val[id] = round(hx[id].get_weight(5))
-        print "current sample:" , id ,val[id]
-        recent_data[id][1:10] = recent_data[id][0:9]
-        recent_data[id][0] =  val[id]
-        print "recent 10 data:", id, recent_data[id]
-        
-        if recent_data[id][5] <= 0 and recent_data[id][4] > 0:
-            unit_weight[id] = most_common(recent_data[id][0:3])
-            
-        real_weight[id] = most_common(recent_data[id])
-        if real_weight[id] <= 1:
-            real_weight[id] = 0
-            
-        weight_refer[id] = most_common(recent_data[id][5:10])
-        if weight_refer[id] <= 1:
-            weight_refer[id] = 0
-            
-        print "id:",id, "real weight:",real_weight, "refer weight:", weight_refer, "unit weight:", unit_weight
-        
-        if unit_weight[id] > 1:
-            unitInBasket[id] = round(real_weight[id] / unit_weight[id])
-            unitRefer[id] = round(weight_refer[id] / unit_weight[id])
-        else:
-            unitInBasket[id] = 0
-            unitRefer[id] = 0
-        print "id:", id, "unit in the basket:", unitInBasket[id], "refer unit:", unitRefer[id]
-        
-        if real_weight[id] > weight_refer[id] and count[id] == 0:
-            total[id] += unitInBasket[id] - unitRefer[id]
-            flag[id] = 1
-            
-        print "subtotal:",id, total[id]
-        print "==============="
-        
-        hx[id].power_down()
-        hx[id].power_up()
-        time.sleep(0.5)
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
 
-if __name__ == "__main__":
-    init(6,5,id)
-    main(id)
+app.run(host='0.0.0.0',port=5000)
+
+    
+    
     
